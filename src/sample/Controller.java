7537087220a296de9fa91c3437797b6243dc25d0
Mario.S <stem63@yahoo.de>
2020-08-02 23:39:32 +0200

    added Label,slider,button e.t.c

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+    public Slider sliderLevel;
+
+
+
+    public void btnStartOnClick(MouseEvent mouseEvent) {
+        System.out.println("Start geklickt");
+        sliderLevel.setId("slideLevel");
+        System.out.println("Level ist: "+sliderLevel.getValue());
+        ConfigGame game = new ConfigGame((int)sliderLevel.getValue());
+    }
 }
