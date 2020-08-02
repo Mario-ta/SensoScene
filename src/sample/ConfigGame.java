6be4332d5773d51a1c786fdc7202712d7542c46d
Mario.S <stem63@yahoo.de>
2020-08-02 23:43:43 +0200

    added Label,slider,button e.t.c

---
+++ src/sample/ConfigGame.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class ConfigGame {
+
+    private int level = 1;
+    private int[] reihe = {1,2,3,4,1,2,3,4};
+
+    public ConfigGame(int level){}
+
+    public int[] getReihe() {
+        return reihe;
+    }
+}
